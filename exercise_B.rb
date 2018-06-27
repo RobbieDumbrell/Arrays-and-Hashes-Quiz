4613d2d1883c34a41988363917ab41f0cec54afa
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-06-27 19:19:06 +0100

    adding answers to exercise_B.rb

--- exercise_B.rb
+++ exercise_B.rb
@@ -58,38 +58,52 @@ users = {
 
 # Get Jonathan's Twitter handle (i.e. the string "jonnyt")
 
-
+jonathan_twitter = users["Jonathan"][:twitter]
+# p jonathan_twitter
 
 # Get Erik's hometown
 
-
+erik_hometown = users["Erik"][:home_town]
+# p erik_hometown
 
 # Get the array of Erik's lottery numbers
 
-
+erik_lottery_numbers = users["Erik"][:lottery_numbers]
+# p erik_lottery_numbers
 
 # Get the type of Avril's pet Monty
 
-
+monty_type = users["Avril"][:pets][0][:species]
+# p monty_type
 
 # Get the smallest of Erik's lottery numbers
 
-
+erik_sorted_lottery_numbers = erik_lottery_numbers.sort
+erik_smallest_lottery_number = erik_sorted_lottery_numbers.first
+# p erik_smallest_lottery_number
 
 # Return an array of Avril's lottery numbers that are even
 
-
+avril_lottery_numbers = users["Avril"][:lottery_numbers]
+avril_even_lottery_numbers = avril_lottery_numbers.select {|number| number.even?}
+# p avril_even_lottery_numbers
 
 # Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
 
-
+users["Erik"][:lottery_numbers] << 7
+# p users["Erik"][:lottery_numbers]
 
 # Change Erik's hometown to Edinburgh
 
-
+users["Erik"][:home_town] = "Edinburgh"
+# p users["Erik"][:home_town]
 
 # Add a pet dog to Erik called "Fluffy"
 
-
+users["Erik"][:pets] << {name: "Fluffy", species: "dog"}
+# p users["Erik"][:pets]
 
 # Add another person to the users hash
+
+users["Robbie"] = {}
+# p users
